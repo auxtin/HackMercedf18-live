dd3871e9c34974f747d28857dbabc39dfd748ad1
Austin <aleonardo2@ucmerced.edu>
2018-09-15 21:27:53 -0700

    imported components into main page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Link, NavLink, Redirect, Prompt } from 'react-router-dom';
 import Route from 'react-router-dom/Route'
+import ReactDOM from 'react-dom';
+
+import notification from './Components/notification';
+import speaker from './Components/speaker';
+import sponsor from './Components/sponsor';
 
 class App extends Component {
   render() {
